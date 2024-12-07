b458b50651c244ac401648737f191ffeea443d2b
danilomarroco <dlmarroco@gmail.com>
2024-12-07 10:30:06 -0300

    adicionando componente de menu sem customizacao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Menu from "./pages/menu/menu";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Menu />
       <ListarTarefa />
     </div>
   );
